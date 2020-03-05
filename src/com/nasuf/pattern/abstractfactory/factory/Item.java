349586aadb078883b7e075a9c89996e12d2000f7
pocketContributor <842100455@qq.com>
2020-03-05 23:40:48 +0800

    bridge

---
+++ src/com/nasuf/pattern/abstractfactory/factory/Item.java
@@ -0,0 +1,13 @@
+package com.nasuf.pattern.abstractfactory.factory;
+
+public abstract class Item {
+
+    protected String caption;
+
+    public Item(String caption) {
+        this.caption = caption;
+    }
+
+    public abstract String makeHTML();
+
+}
